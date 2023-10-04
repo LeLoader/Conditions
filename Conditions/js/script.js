b5d7c1fa898113870210d35ac7ad422813900084
LeLoader <61639484+LeLoader@users.noreply.github.com>
2023-10-04 11:03:57 +0200

    Ex conditions

---
+++ Conditions/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function Conditions(){
+    let a = 4;
+    let b = 4;
+    let c = 3;
+
+    if (a==b){
+        console.log("a = b");
+    }
+    
+    else if (a==c){
+        console.log("a = c");
+    }
+
+    else{
+        console.log("égal à rien");
+    }
+}
+
+Conditions()
